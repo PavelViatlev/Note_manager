43bd9359b91f743ef0608513a96010cac211b77d
PavelViatlev <viatlevpa@gmail.com>
2024-12-10 18:24:34 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,16 @@
+# Input Data from user
+Username = input('Введите имя пользователя: ') # Имя пользователя
+print('Username:', Username)
+Title = input('Введите заголовок заметки: ') # Заголовок заметки
+print('Title:', Title)
+Content = input('Введите описание заметки: ') # Описание заметки
+print('Content:', Content)
+Status = input('Введите статус заметки: ') # Статус заметки
+print('Status:', Status)
+Created_date = input('Введите дату создания заметки в формате-"день.месяц.год": ')
+print('Created date:', Created_date)
+#Дата создания заметки в формате-"день.месяц.год"
+Issue_date = input('Введите дату истечения заметки (дедлайн)в формате-"день.месяц.год": ')
+#Дата истечения заметки (дедлайн)в формате-"день.месяц.год"
+print('Issue date:', Issue_date)
+
