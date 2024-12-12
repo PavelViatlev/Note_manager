13026cd17981fcf18973fa91b7c20682264bafa1
PavelViatlev <viatlevpa@gmail.com>
2024-12-12 17:48:13 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -2,10 +2,7 @@ from datetime import datetime as dt
 created_date = "19.01.2024"
 issue_date = "19.05.2024"
 temp_created_date = dt.strptime(created_date, '%d.%m.%Y')
-#print(created_date, temp_created_date)
 temp_issue_date = dt.strptime(issue_date, '%d.%m.%Y')
-#print(issue_date, temp_issue_date)
-
 created_date = temp_created_date.strftime('%d.%m.')
 issue_date=temp_issue_date.strftime('%d.%m.')
 print('Сreated date:', created_date)
