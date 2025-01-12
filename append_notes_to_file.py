35dbdb5f9f77c93fa641794731fd112aa65cc7e8
PavelViatlev <viatlevpa@gmail.com>
2025-01-12 13:49:10 +0300

    Add files via upload

--- append_notes_to_file.py
+++ append_notes_to_file.py
@@ -10,17 +10,13 @@ notes_add = [
      'Дата создания': '20.11.2024', 'Дедлайн': '26.11.2024'},
 ]
 
-class IncorrectData(Exception): # создаем класс для обработки ошибок
-    pass
 
 def append_notes_to_file(notes, filename):
     try:  # для обработки исключения файл не найден - FileNotFoundError
         with open(filename, 'r', encoding='utf-8') as file:  # блок для автоматического закрытия файла
             i = 0
             temp_str = file.readlines()  # временная строка
-            if len(temp_str) == 0:  # Проверка пустого файла
-                raise IncorrectData("\nФайл пуст. Необходимо добавить заметки в файл.\nРабота программы завершена.")
-            else:
+            if len(temp_str) != 0:  # Проверка пустого файла
                 file.seek(0)  # текущую позицию на начало файла
                 while bool(temp_str): # цикл поиска количества строк
                     i += 1
@@ -36,16 +32,15 @@ def append_notes_to_file(notes, filename):
                 temp_str = temp_str[2].split('.')  # третью строку списка делим строку по признаку - '.'
                 quant_note_file = temp_str[0] # кол-во заметок в файле
                 quant_note_file = int(quant_note_file)
+            else:
+                quant_note_file = 0
         with open(filename, 'a+', encoding='utf-8') as file:  # блок для автоматического закрытия файла
-            print(notes_add)
+
             for i in range(len(notes_add)):
                 file.write(f'Заметка № {quant_note_file + i + 1}.\n')
                 for key, value in notes_add[i].items():
                     file.write (f"{key}: {value}\n")
                 file.write("-------------------------------------\n")
-
-    except IncorrectData as e:  # обработка исключения при пустом  или неправильном формате файла
-        print(e)
 
     except FileNotFoundError: # обработка исключения при невозможности открытия файла
         print("Файл 'Notes Manager.txt' не найден.")
